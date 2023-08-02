8316ecf6c76b74a4a8230a6e19d1359ab34b4b2b
PearlZuoYiwei <141213025+PearlZuoYiwei@users.noreply.github.com>
2023-08-02 15:36:40 +0800

    Update pinecone_询问知识库.py

--- pinecone_询问知识库.py
+++ pinecone_询问知识库.py
@@ -1,9 +1,12 @@
-
 from langchain.vectorstores import Chroma,Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
 import os
-os.environ["OPENAI_API_KEY"] = "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"
+import streamlit as st
+st.title('开山股份本地知识库')
+user_input = st.text_input("请输入你想了解的关于开山股份的信息（输入p时结束）：")
+OPENAI_API_KEY=st.text_input("请输入你的OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 embeddings=OpenAIEmbeddings()
 pinecone.init(api_key='a4dfd6f5-79ce-438c-8628-855ef4d8480b',environment='us-west4-gcp-free')
@@ -25,9 +28,7 @@ vectorstore_info = VectorStoreInfo(
 )
 toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
 agent_executor = create_vectorstore_agent(llm=llm, toolkit=toolkit, verbose=True)
-import streamlit as st
-st.title('开山股份本地知识库')
-user_input = st.text_input("请输入你想了解的关于开山股份的信息（输入p时结束）：")
+
 st.write(agent_executor.run(user_input))
 # while True:
 #     user_input = input("请输入你想了解的关于开山股份的信息（输入p时结束）：")
